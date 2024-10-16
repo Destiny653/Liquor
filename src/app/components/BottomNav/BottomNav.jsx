1d860559103ec235e40d23c418e976594f034a62
Destiny653 <fokundem.com@gmail.com>
2024-10-16 14:20:52 +0100

    **

--- src/app/components/BottomNav/BottomNav.jsx
+++ src/app/components/BottomNav/BottomNav.jsx
@@ -19,7 +19,7 @@ export default function BottomNav() {
             <div onClick={()=>navigation.push('/cart')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><HiOutlineShoppingCart className="inline msgBtn" size={25} /></div>
             <div onClick={()=>navigation.push('/shop')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><BsShop className="inline msgBtn" size={25}/></div>
             <div onClick={()=>navigation.push('/')} className="icon w-[40px] h-[40px] rounded-full flex items-center justify-center"><TfiHome size={28} /></div>
-            <div onClick={()=>navigation.push('/register')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><IoIosLogIn className="inline msgBtn" size={25} /></div>
+            <div onClick={()=>navigation.push('/signup')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><IoIosLogIn className="inline msgBtn" size={25} /></div>
             <div onClick={() => !msgBtn ? setMsgBtn(true) : setMsgBtn(false)} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><AiOutlineMessage  className="inline msgBtn" size={25} /></div>
         </div>
     )
