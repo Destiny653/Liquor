46497acbcbfc2fc659d5b32f454476871179d0f8
Destiny653 <fokundem.com@gmail.com>
2024-10-16 14:11:26 +0100

    gg

--- src/app/components/BottomNav/BottomNav.jsx
+++ src/app/components/BottomNav/BottomNav.jsx
@@ -1,7 +1,9 @@
 'use client'
 import React, { useContext } from 'react'
-import { AiFillMessage } from 'react-icons/ai';
+import { AiOutlineMessage } from "react-icons/ai";
 import { HiOutlineShoppingCart } from "react-icons/hi2";
+import { IoIosLogIn } from "react-icons/io";
+import { BsShop } from "react-icons/bs";
 import { SearchContext } from '../../../../context/SearchContext';
 import './bottom.css'
 import Contact from '@/app/contact/page';
@@ -14,11 +16,11 @@ export default function BottomNav() {
     return (
         <div className='icon-parent'>
             {msgBtn && <Contact />}
-            <div onClick={()=>navigation.push('/cart')} className="icon w-[35px] h-[35px] rounded-full bg-white flex items-center justify-center"><HiOutlineShoppingCart className="inline msgBtn" size={25} /></div>
-            <div className="icon w-[35px] h-[35px] rounded-full bg-white flex items-center justify-center"></div>
-            <div onClick={()=>navigation.push('/')} className="icon w-[40px] h-[40px] rounded-full bg-white flex items-center justify-center"><TfiHome size={28} className='text-[#000000]' /></div>
-            <div className="icon w-[35px] h-[35px] rounded-full bg-white flex items-center justify-center"></div>
-            <div onClick={() => !msgBtn ? setMsgBtn(true) : setMsgBtn(false)} className="icon w-[35px] h-[35px] rounded-full bg-white flex items-center justify-center"><AiFillMessage className="inline msgBtn" size={25} /></div>
+            <div onClick={()=>navigation.push('/cart')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><HiOutlineShoppingCart className="inline msgBtn" size={25} /></div>
+            <div onClick={()=>navigation.push('/shop')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><BsShop className="inline msgBtn" size={25}/></div>
+            <div onClick={()=>navigation.push('/')} className="icon w-[40px] h-[40px] rounded-full flex items-center justify-center"><TfiHome size={28} /></div>
+            <div onClick={()=>navigation.push('/register')} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><IoIosLogIn className="inline msgBtn" size={25} /></div>
+            <div onClick={() => !msgBtn ? setMsgBtn(true) : setMsgBtn(false)} className="icon w-[35px] h-[35px] rounded-full flex items-center justify-center"><AiOutlineMessage  className="inline msgBtn" size={25} /></div>
         </div>
     )
 }
