9ecc5d19f8fe34f79dea9282d0998565515df12b
Destiny653 <fokundem.com@gmail.com>
2024-10-20 11:33:26 +0100

    ***

--- src/app/components/Quantity/quantity.jsx
+++ src/app/components/Quantity/quantity.jsx
@@ -4,7 +4,9 @@ import React from 'react'
 export default function Qty({ productId }) {
     // if(typeof window !== 'undefined'){}
     const localItems = typeof window !== 'undefined' && JSON.parse(localStorage.getItem('cartItems'))
-    const items = localItems?.find((item) => item.product_id === productId)
+    const items = localItems && localItems.find((item) => item.product_id === productId)
+    if (!items) return null
+    
     return (
         <div className='cart-item-display flex items-center justify-center'>
             <Link href={'/cart'}>
